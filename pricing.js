5fe3feb6dbe9f68b15dcfbbd05e856998295c570
Patrick O'Connor <poc.967@gmail.com>
2019-09-16 18:51:58 -0400

    defined smaller functions outside of switch and invoked them where needed

--- pricing.js
+++ pricing.js
@@ -2,35 +2,43 @@ module.exports.calculateProductPrice = function (product, employee, coverageLeve
   var price = 0
   var dollarsOff = 0
 
+  function volLifePrice() {
+    for (var i = 0; i < coverageLevels.length; i++) {
+      var coverageAmount = coverageLevels[i].coverage
+
+      price += (coverageAmount / product.cost.costDivisor) * product.cost.price
+    }
+
+    if (product.employerContribution.mode === 'dollar') {
+      price = price - product.employerContribution.contribution
+    } else {
+      dollarsOff = price * (product.employerContribution.contribution / 100)
+      price = price - dollarsOff
+    }
+    return parseInt(price * 100) / 100
+  }
+
+  function ltdLifePrice() {
+    var salaryPercentage = product.coveragePercentage / 100
+
+    price += ((employee.salary * salaryPercentage) / product.cost.costDivisor) * product.cost.price
+
+    if (product.employerContribution.mode === 'dollar') {
+      price = price - product.employerContribution.contribution
+    } else {
+      dollarsOff = price * product.employerContribution.contribution
+      price = price - dollarsOff
+    }
+    return parseInt(price * 100) / 100
+  }
+
   switch (product.type) {
     case 'volLife':
-      for (var i = 0; i < coverageLevels.length; i++) {
-        var coverageAmount = coverageLevels[i].coverage
+      return volLifePrice()
 
-        price += (coverageAmount / product.cost.costDivisor) * product.cost.price
-      }
-
-      if (product.employerContribution.mode === 'dollar') {
-        price = price - product.employerContribution.contribution
-      } else {
-        dollarsOff = price * (product.employerContribution.contribution / 100)
-        price = price - dollarsOff
-      }
-
-      return parseInt(price * 100) / 100
     case 'ltd':
-      var salaryPercentage = product.coveragePercentage / 100
+      return ltdLifePrice()
 
-      price += ((employee.salary * salaryPercentage) / product.cost.costDivisor) * product.cost.price
-
-      if (product.employerContribution.mode === 'dollar') {
-        price = price - product.employerContribution.contribution
-      } else {
-        dollarsOff = price * product.employerContribution.contribution
-        price = price - dollarsOff
-      }
-
-      return parseInt(price * 100) / 100
     default:
       return 0
   }
